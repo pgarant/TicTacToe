d7936957b4b14d953494ed614e0d5b34cb8a84f9
Pierre Garant <garant.pierre@mac.com>
2021-10-02 21:27:36 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Pierre Garant on 2021-10-02.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
