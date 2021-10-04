9ab70cd32c54b644bba34f19178684710454ff9d
Pierre Garant <garant.pierre@mac.com>
2021-10-03 22:51:08 -0400

    End Of Lesson 12

--- TicTacToe/Views/HomeView/GameViewModel.swift
+++ TicTacToe/Views/HomeView/GameViewModel.swift
@@ -12,31 +12,43 @@ final class GameViewModel: ObservableObject{
     
     @Published var game = Game(id: UUID().uuidString, player1Id: "Player 1", player2Id: "Player 2", blockMoveForPlayerId: "Player 2", winningPlayerId: "", rematchPlayerId: [], moves: Array(repeating: nil, count: 9) )
     
-    private let winPattern: Set<Set<Int>> = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+    private let winPatterns: Set<Set<Int>> = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
         
     func processPlayerMove (for position: Int, isPlayer1: Bool) {
 
         if isSquareOccupied(in: game.moves, forIndex: position) { return }
         
         game.moves[position] = Move(isPlayer1: isPlayer1, boardIndex: position )
-
+        game.blockMoveForPlayerId = "PLayer2"
+        
         // block the move
         
-        // check for win*
-        
+        // check for win
+        if checkForWinCondition(for: true, in: game.moves) {
+            print ("You have won!")
+            return
+        }
         
         // check for draw
-
+        if checkForDraw(in: game.moves) {
+            print ("Draw!")
+            return
+        }
     }
     
     func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
         return moves.contains (where: { $0?.boardIndex == index} )
     }
     
-    func checkForWinCondition() -> Bool {
+    func checkForWinCondition(for player1:Bool, in moves: [Move?]) -> Bool {
         
+        // remove all nils
+        let playerMoves = moves.compactMap {$0}.filter { $0.isPlayer1 == player1 }
+        let playerPositions = Set( playerMoves.map { $0.boardIndex } )
         
-        return true
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions ) { return true }
+        
+        return false
         
     }
     
