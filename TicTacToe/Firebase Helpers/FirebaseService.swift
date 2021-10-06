176c4948966164e7e4ea87d05b73ec9384b11456
Pierre Garant <garant.pierre@mac.com>
2021-10-05 22:42:51 -0400

    FIN

--- TicTacToe/Firebase Helpers/FirebaseService.swift
+++ TicTacToe/Firebase Helpers/FirebaseService.swift
@@ -23,18 +23,21 @@ final class FirebaseService: ObservableObject{
     }
     
     func startGame(with userId: String){
-        
+        FirebaseReference(.Game).whereField("player2Id", isEqualTo: "").whereField("player1Id", isNotEqualTo: userId).getDocuments(FIRQuerySnapshot, error IN
+        ) { <#QuerySnapshot?#>, <#Error?#> in
+            <#code#>
+        }
     }
     
     func listenForGameChanges(){
         
     }
     
-    func createNewGame (with userId: String){
+    func createNewGame(with userId: String){
         
     }
     
-    func quitTheGame (){
+    func quitTheGame(){
         
     }
 }
