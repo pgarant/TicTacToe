75b4b2eb638331153f56268b32b8ea1abb096f29
Pierre Garant <garant.pierre@mac.com>
2021-10-06 05:54:44 -0400

    End of Lesson 12

--- TicTacToe/Firebase Helpers/FirebaseService.swift
+++ TicTacToe/Firebase Helpers/FirebaseService.swift
@@ -19,14 +19,41 @@ final class FirebaseService: ObservableObject{
     }
     
     func createOnlineGame() {
+        // Save the game online
         
+        do {
+            try FirebaseReference(.Game).document(self.game.id).setData(from: self.game)
+        } catch {
+            print ("Error creating online game",error.localizedDescription)
+        }
     }
     
     func startGame(with userId: String){
-        FirebaseReference(.Game).whereField("player2Id", isEqualTo: "").whereField("player1Id", isNotEqualTo: userId).getDocuments(FIRQuerySnapshot, error IN
-        ) { <#QuerySnapshot?#>, <#Error?#> in
-            <#code#>
+        FirebaseReference(.Game).whereField("player2Id", isEqualTo: "").whereField("player1Id",isNotEqualTo: userId).getDocuments{querySnapshot, error in
+
+            if error != nil {
+                print ("Error starting game",error?.localizedDescription)
+                //  Create new game
+                self.createNewGame(with: userId)
+                return
+            }
+            
+            // If at least a game already exist
+            if let gameData = querySnapshot?.documents.first {
+
+                self.game = try? gameData.data(as: Game.self)
+                self.game.player1Id = userId
+                self.game.blockMoveForPlayerId = userId
+                
+                self.updateGame(self.game)
+                self.listenForGameChanges()
+            } else {
+                // Create a new game
+                self.createNewGame(with: userId)
+            }
+            
         }
+        
     }
     
     func listenForGameChanges(){
@@ -34,6 +61,14 @@ final class FirebaseService: ObservableObject{
     }
     
     func createNewGame(with userId: String){
+        print ("creating game for userid", userId)
+        
+        self.game = Game(id: UUID().uuidString, player1Id: userId, player2Id: "", blockMoveForPlayerId: userId, winningPlayerId: "", rematchPlayerId: [], moves: Array(repeating: nil, count: 9))
+        self.createOnlineGame()
+        self.listenForGameChanges()
+    }
+    
+    func updateGame(_ game:Game) {
         
     }
     
